e1becf8d83254aefec93cc606095a414ad1b02dd
200111521 <200111521@comp6.hwccluster.labcenter>
2022-10-19 15:44:11 +0000

    实验三第二次修改

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -18,15 +18,24 @@ struct run {
   struct run *next;
 };
 
-struct {
+struct kmem{
   struct spinlock lock;
   struct run *freelist;
 } kmem;
 
+/*为每个cpu维护一个freelist,每一个freelist都有独自的lock*/
+struct kmem CPU_kmems[NCPU];
+
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  // initlock(&kmem.lock, "kmem");
+
+  /*初始化NCPU个锁*/
+  for(int i=0;i<NCPU;i++){
+    initlock(&CPU_kmems[i].lock,"kmem");
+  }
+
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -38,6 +47,8 @@ freerange(void *pa_start, void *pa_end)
   for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
     kfree(p);
 }
+
+
 
 // Free the page of physical memory pointed at by v,
 // which normally should have been returned by a
@@ -56,9 +67,16 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
+  /*获取当前运行的CPU号*/
+  push_off();
+  int now_CPU_id=cpuid();
+  pop_off();
+  
+  /*头插法把被释放的块插进相应的freelist*/
+  acquire(&CPU_kmems[now_CPU_id].lock);
+  r->next = CPU_kmems[now_CPU_id].freelist;
+  CPU_kmems[now_CPU_id].freelist = r;
+  
   release(&kmem.lock);
 }
 
@@ -68,15 +86,52 @@ kfree(void *pa)
 void *
 kalloc(void)
 {
-  struct run *r;
+  // struct run *r = kmem.freelist;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+  /*获取当前运行的CPU号*/
+  push_off();
+  int now_CPU_id=cpuid();
+  pop_off();
+
+  acquire(&CPU_kmems[now_CPU_id].lock);
+  struct run *r = CPU_kmems[now_CPU_id].freelist;
+  if(r)/*有空内存直接用*/
+    CPU_kmems[now_CPU_id].freelist = r->next;
+  else/*偷内存*/
+  {
+    for (int i = 0; i < 8; i++)
+    {
+      if(i!=now_CPU_id){
+        acquire(&CPU_kmems[i].lock);
+        r=CPU_kmems[i].freelist;
+        if(r){/*有空内存，进行窃取*/
+          CPU_kmems[i].freelist=r->next;
+          release(&CPU_kmems[i].lock);
+          break;
+        }
+        release(&CPU_kmems[i].lock);
+      }
+      else
+      {
+        continue;
+      }
+      
+    }
+    
+  }
+  release(&CPU_kmems[now_CPU_id].lock);
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
+}
+
+uint64 free_mem(void){
+  int len=0;
+  struct run *run_pointer=kmem.freelist;;
+  while(run_pointer){
+    len++;
+    run_pointer=run_pointer->next;
+  }
+  return len*PGSIZE;
 }
